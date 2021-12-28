f4bc2848be9f1dd68aff914a8bc5273cc2d6f885
Aislan Santos <aislan_85@yahoo.com.br>
2021-12-28 15:06:23 -0300

    criação de login - app telas view e urls

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
+
 urlpatterns =[
     path('login/', auth_views.LoginView.as_view(template_name='usuarios/login.html', extra_context={'titulo':'Autenticação'}), name='login'),
 ]
